ad62af207cacdbf740f4b8a58a2918189702b42e
MaxOhn <ohn.m@hotmail.de>
2018-09-05 23:15:18 +0200

    Added OsuModReveal class and adjusted OsuRuleset.cs

--- osu.Game.Rulesets.Osu/OsuRuleset.cs
+++ osu.Game.Rulesets.Osu/OsuRuleset.cs
@@ -117,6 +117,11 @@ namespace osu.Game.Rulesets.Osu
                         new OsuModRelax(),
                         new OsuModAutopilot(),
                     };
+                case ModType.Fun:
+                    return new Mod[]
+                    {
+                        new OsuModReveal(),
+                    };
                 default:
                     return new Mod[] { };
             }
