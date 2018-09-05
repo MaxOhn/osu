f0cc1b81b8e9c6de4a03d3a8f225f35931aca794
MaxOhn <ohn.m@hotmail.de>
2018-09-06 00:36:32 +0200

    Added OsuModShrink class and adjusted OsuRuleset.cs

---
+++ osu.Game.Rulesets.Osu/Mods/OsuModShrink.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Graphics;
+using osu.Game.Graphics;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
+using System.Collections.Generic;
+using OpenTK;
+
+namespace osu.Game.Rulesets.Osu.Mods
+{
+    public class OsuModShrink : Mod, IApplicableToDrawableHitObjects
+    {
+        public override string Name => "Shrink";
+        public override string ShortenedName => "SK";
+        public override FontAwesome Icon => FontAwesome.fa_bullseye;
+        public override ModType Type => ModType.Fun;
+        public override string Description => "Is it me or are they getting smaller?";
+        public override double ScoreMultiplier => 1;
+
+        private int amountDrawables;
+
+        public void ApplyToDrawableHitObjects(IEnumerable<DrawableHitObject> drawables)
+        {
+            int counter = 0;
+            foreach (var drawable in drawables)
+            {
+                counter++;
+                drawable.ApplyCustomUpdateState += drawableOnApplyCustomUpdateState;
+            }
+            amountDrawables = counter;
+        }
+
+        protected void drawableOnApplyCustomUpdateState(DrawableHitObject drawable, ArmedState state)
+        {
+            if (!(drawable is DrawableOsuHitObject d))
+                return;
+
+            d.ScaleTo(new Vector2(d.Scale.X*amountDrawables/(amountDrawables + d.HitObject.ComboIndex)));
+        }
+    }
+}
--- osu.Game.Rulesets.Osu/OsuRuleset.cs
+++ osu.Game.Rulesets.Osu/OsuRuleset.cs
@@ -117,6 +117,11 @@ namespace osu.Game.Rulesets.Osu
                         new OsuModRelax(),
                         new OsuModAutopilot(),
                     };
+                case ModType.Fun:
+                    return new Mod[]
+                    {
+                        new OsuModShrink(),
+                    };
                 default:
                     return new Mod[] { };
             }
